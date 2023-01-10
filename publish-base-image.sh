1e7ce38bfc661d6f195b7a5b8e7143eaa744aa1b
Nils Druyen <nils.druyen@fressnapf.com>
2023-01-10 12:35:15 +0100

    Modify publish-base-image.sh

--- publish-base-image.sh
+++ publish-base-image.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# build docker image
 docker build . -f android-base.Dockerfile -t nilsjrfn/android-sdk-base:latest
 
+# publish to docker
 docker push nilsjrfn/android-sdk-base:latest
