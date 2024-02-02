7fad6a5f2deb7dcf7df787a9be4730c759c70514
Nils Druyen <nils.druyen@fressnapf.com>
2024-02-02 09:12:53 +0100

    Fix push naming

--- publish-emulator-image.sh
+++ publish-emulator-image.sh
@@ -4,4 +4,4 @@
 API_LEVEL=$1
 
 docker build . -f android-emulator.Dockerfile --build-arg API_LEVEL=${API_LEVEL} -t nilsjrfn/android-emulator:latest
-docker push nilsjrfn/android-emulator:latest
+docker push nilsjrfn/android-emulator-${API_LEVEL}:latest
--- publish-sdk-image.sh
+++ publish-sdk-image.sh
@@ -4,4 +4,4 @@
 API_LEVEL=$1
 
 docker build . -f android-sdk.Dockerfile --build-arg API_LEVEL=${API_LEVEL} -t nilsjrfn/android-sdk:latest
-docker push nilsjrfn/android-sdk:latest
+docker push nilsjrfn/android-sdk-${API_LEVEL}:latest
