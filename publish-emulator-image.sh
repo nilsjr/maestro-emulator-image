2bd793351fb989fc3d102f04157df3182e079dd3
Nils Druyen <nils.druyen@fressnapf.com>
2024-02-02 09:14:39 +0100

    Fix tag naming

--- publish-emulator-image.sh
+++ publish-emulator-image.sh
@@ -3,5 +3,5 @@
 # API level passed in as argument
 API_LEVEL=$1
 
-docker build . -f android-emulator.Dockerfile --build-arg API_LEVEL=${API_LEVEL} -t nilsjrfn/android-emulator:latest
+docker build . -f android-emulator.Dockerfile --build-arg API_LEVEL=${API_LEVEL} -t nilsjrfn/android-emulator-${API_LEVEL}:latest
 docker push nilsjrfn/android-emulator-${API_LEVEL}:latest
