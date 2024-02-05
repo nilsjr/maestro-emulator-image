8517ae686e95563de8fa058175373a099931cdd4
Nils Druyen <nils.druyen@fressnapf.com>
2024-02-05 11:57:20 +0100

    Push new image

--- publish-emulator-v2-image.sh
+++ publish-emulator-v2-image.sh
@@ -4,4 +4,4 @@
 API_LEVEL=$1
 
 docker build . -f android-emulator-v2.Dockerfile --build-arg API_LEVEL=${API_LEVEL} -t nilsjrfn/android-emulator-${API_LEVEL}:latest
-#docker push nilsjrfn/android-emulator-${API_LEVEL}:latest
+docker push nilsjrfn/android-emulator-${API_LEVEL}:latest
