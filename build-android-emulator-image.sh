61d938ef415973fd77e72688b306fc3dc7b913ee
Nils Druyen <nils.druyen@fressnapf.com>
2024-06-17 10:03:49 +0200

    Edit matrix

--- build-android-emulator-image.sh
+++ build-android-emulator-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build . -f android-emulator.Dockerfile -t nilsjrfn/android-emulator-34:latest
+docker build . -f android-emulator.Dockerfile -t nilsjrfn/android-emulator-34:latest --platform linux/x86_64
 #docker push nilsjrfn/android-emulator-34:latest
